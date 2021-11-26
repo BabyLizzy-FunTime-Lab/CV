a582c2787df8bb040b7a8808881fc6a665bc4d16
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2021-11-26 16:49:22 +0100

    added an eventlistener to experience, ready to write shrink func, can now detect IE

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+// Check for internet explorer
+if (window.document.documentMode) {
+	alert("please use a modern browser");
+}
+// global functions
+function elementClass(classname) {
+	return document.getElementsByClassName(classname);
+}
+
+
+// elementClass("resume-experience-info").addEventListener("click", function(event) {
+// 	event.preventDefault();
+// 	this.style.backgroundColor = "red";
+// })
+
+function showExperience() {
+	let classArray = elementClass("resume-experience-info");
+	console.log(classArray);
+
+	Array.from(classArray).forEach(function(element) {
+		element.addEventListener("click", function(event) {
+			this.style.backgroundColor = "red";
+		})
+	})
+
+}
+showExperience();
