32173ffc5fdb135c6175e2028d93724f2ecfb76a
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2021-12-02 16:53:57 +0100

    fucntion still wierd, else if in not working properly

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,15 @@
 if (window.document.documentMode) {
 	alert("Please use a modern browser");
 }
+// global variabels
+let clickedExperience = false;
+
 // global functions
 function elementClass(classname) {
 	return document.getElementsByClassName(classname);
+}
+function elementID(id) {
+	return document.getElementById(id);
 }
 
 // (function showExperience() {
@@ -26,11 +32,26 @@ function showExperience() {
 
 	classArray.forEach(function(element) {
 		element.addEventListener("click", function(event) {
-			classArray.forEach(function(element){
-				element.style.height = "2.5em";
-			})
-			this.style.height = "auto";
-			console.log(this.className);
+
+
+
+			if (clickedExperience === this.id) {
+				clickedExperience = false;
+				this.style.height = "2.5em";
+			} else if (clickedExperience !== this.className && clickedExperience !== false){
+				// oldclicked goes small
+				elementID(clickedExperience).style.height = "2.5em";
+				// newclicked goes big
+				this.style.height = "auto";
+				// let is replaced
+				clickedExperience = this.id;
+			} else {
+				// classArray.forEach(function(element){
+				// element.style.height = "2.5em";
+				// })
+				clickedExperience = this.id;
+				this.style.height = "auto";
+			}
 		})
 	})
 };
